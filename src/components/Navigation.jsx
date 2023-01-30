e67d70bc193702ca91a9605e96c915e9e21959a9
SandeSydney <sandesydney@gmail.com>
2023-01-30 15:57:09 +0300

    Added links to pages

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import brand from "../assets/Brand.png"
 
 function Navigation() {
   return (
-    <div>Navigation</div>
+    <nav className="bg-white-500 flex py-5 items-center">
+      <div className='flex-1 flex gap-10'>
+        <Link to={"/"}>
+          <img style={{ maxWidth: "200px" }} src={brand} alt="Logo" />
+        </Link>
+      </div>
+      <div className="flex flex-1 justify-evenly">
+        <Link to={""}>Home</Link>
+        <Link to={""}>Services</Link>
+        <Link to={""}>Portfolio</Link>
+        <Link to={""}>Contact</Link>
+      </div>
+    </nav>
   )
 }
 
