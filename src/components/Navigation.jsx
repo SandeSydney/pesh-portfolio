d981f2bf58f957bcc49b859b096800320d51cc3e
SandeSydney <sandesydney@gmail.com>
2023-01-30 16:49:57 +0300

    Styled links and hover effects

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,17 +4,25 @@ import brand from "../assets/Brand.png"
 
 function Navigation() {
   return (
-    <nav className="bg-white-500 flex py-5 items-center">
+    <nav className="relative border-b-2 mb-3 flex py-5 items-center">
       <div className='flex-1 flex gap-10'>
         <Link to={"/"}>
           <img style={{ maxWidth: "200px" }} src={brand} alt="Logo" />
         </Link>
       </div>
-      <div className="flex flex-1 justify-evenly">
-        <Link to={""}>Home</Link>
-        <Link to={""}>Services</Link>
-        <Link to={""}>Portfolio</Link>
-        <Link to={""}>Contact</Link>
+      <div className="hidden md:flex flex-1 justify-evenly">
+        <Link to={""} className={"hover:text-red-600 hover:underline hover:underline-offset-8"}>
+          Home
+        </Link>
+        <Link to={""} className={"hover:text-red-600 hover:underline hover:underline-offset-8"}>
+          Services
+        </Link>
+        <Link to={""} className={"hover:text-red-600 hover:underline hover:underline-offset-8"}>
+          Portfolio
+        </Link>
+        <Link to={""} className={"hover:text-red-600 hover:underline hover:underline-offset-8"}>
+          Contact
+        </Link>
       </div>
     </nav>
   )
