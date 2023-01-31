4d19fe2e805e4dc0be1d09a267ca6b1c1444dfc6
SandeSydney <sandesydney@gmail.com>
2023-01-31 12:47:28 +0300

    Styled Navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,23 +4,23 @@ import brand from "../assets/Brand.png"
 
 function Navigation() {
   return (
-    <nav className="relative border-b-2 mb-3 flex py-5 items-center">
+    <nav className="relative border-b-2 flex py-5 items-center">
       <div className='flex-1 flex gap-10'>
         <Link to={"/"}>
-          <img style={{ maxWidth: "200px" }} src={brand} alt="Logo" />
+          <img className='mx-3 w-1/3' src={brand} alt="Logo" />
         </Link>
       </div>
       <div className="hidden md:flex flex-1 justify-evenly">
-        <Link to={""} className={"hover:text-red-600 hover:underline hover:underline-offset-8"}>
+        <Link to={""} className={"hover:text-[#ee2328] hover:underline hover:underline-offset-8"}>
           Home
         </Link>
-        <Link to={""} className={"hover:text-red-600 hover:underline hover:underline-offset-8"}>
+        <Link to={""} className={"hover:text-[#ee2328] hover:underline hover:underline-offset-8"}>
           Services
         </Link>
-        <Link to={""} className={"hover:text-red-600 hover:underline hover:underline-offset-8"}>
+        <Link to={""} className={"hover:text-[#ee2328] hover:underline hover:underline-offset-8"}>
           Portfolio
         </Link>
-        <Link to={""} className={"hover:text-red-600 hover:underline hover:underline-offset-8"}>
+        <Link to={""} className={"hover:text-[#ee2328] hover:underline hover:underline-offset-8"}>
           Contact
         </Link>
       </div>
