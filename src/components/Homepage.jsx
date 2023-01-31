e773bf7137d41f8870cd666ef6b7e937f366834e
SandeSydney <sandesydney@gmail.com>
2023-01-31 14:23:44 +0300

    Changed background properties

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -15,13 +15,15 @@ function Homepage() {
                 <a className='border text-white mb-20 px-5 py-2 rounded uppercase animate-bounce hover:px-8 hover:animate-none' href="">Let's Talk</a>
             </section>
 
-            <section className="bg-[#F7F7F7]p-20 min-h-screen flex flex-col justify-center items-center space-y-8 text-lg px-8">
+            <section className="bg-[#f0f0f0] p-20 min-h-screen flex flex-col justify-center items-center space-y-8 text-lg px-8">
                 <h2 className='text-4xl text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h2>
                 <hr className='w-12 h-2 bg-[#F46624] rounded'/>
                 <p className='max-w-3xl'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                 <p className='max-w-3xl'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                 <p className='max-w-3xl'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
             </section>
+
+            <section className='min-h-screen bg-white'></section>
         </>
     )
 }
